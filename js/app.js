1faaf7e166366af07a372471195444ea3c8f57f9
helenharnisch <harnischvioleta@gmail.com>
2017-11-20 23:19:25 -0300

    Trabajo Dashboard

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
-// Primer grafico
+
+// Primer grafico enrollment
+
 Morris.Bar({
   element: 'graph-enrollment',
   data: [
@@ -21,7 +23,7 @@ Morris.Bar({
   hideHover: 'always',
 });
 
-//Segundo grafico
+//Segundo grafico achievement
 
 Morris.Donut({
   element: 'graph-achievement',
@@ -40,7 +42,7 @@ Morris.Donut({
   formatter: function (x) { return x + "%"}
 });
 
-//Tercer parrafo
+//Tercer parrafo net promoter score
 
 Morris.Donut({
   element: 'graph-promoter',
@@ -60,14 +62,13 @@ Morris.Donut({
   formatter: function (x) { return x + "%"}
 });
 
-//Cuarto grafico
+//Cuarto grafico tech skills
 
 Morris.Donut({
   element: 'graph-tech',
   data: [
     {value: 62, label: 'COMPLY'},
     {value: 38, label: 'FAILS'},
-
   ],
   backgroundColor: '#ccc',
   labelColor: '#faa719',
@@ -80,14 +81,13 @@ Morris.Donut({
   formatter: function (x) { return x + "%"}
 });
 
-//Quinto Grafico
+//Quinto Grafico life skills
 
 Morris.Donut({
   element: 'graph-life',
   data: [
     {value: 62, label: 'COMPLY'},
     {value: 38, label: 'FAILS'},
-
   ],
   backgroundColor: '#ccc',
   labelColor: '#faa719',
@@ -100,86 +100,134 @@ Morris.Donut({
   formatter: function (x) { return x + "%"}
 });
 
-//ultimo grafico
+//Sexto grafico students satisfaction
 
 Morris.Donut({
   element: 'graph-satisfaction',
   data: [
-    {value: 70, label: 'foo'},
-    {value: 15, label: 'bar'},
-    {value: 10, label: 'baz'},
-    {value: 5, label: 'A really really long label'}
+    {value: 140, label: 'CUMPLY'},
+    {value: 15, label: 'FAILS'},
   ],
   backgroundColor: '#ccc',
-  labelColor: '#060',
+  labelColor: '#faa719',
   colors: [
-    '#0BA462',
-    '#39B580',
-    '#67C69D',
+    '#faa719',
+    '#ec6a0c',
+    '#f91b05',
     '#95D7BB'
   ],
   formatter: function (x) { return x + "%"}
 });
 
-/*var container = document.getElementById('container');
-console.log(container);
+//Séptimo grafico teacher rating
 
-for (var i = 0; i < data.SCL['2016-2'].students.length; i++) {
-	console.log(data.SCL['2016-2'].students[i].name);
-	var parrafo = document.createElement('p');
-	var nodoParrafo = document.createTextNode('La estudiante número ' + [i+1] + ' es: ' + data.SCL['2016-2'].students[i].name);
-		parrafo.appendChild(nodoParrafo);
-		container.appendChild(parrafo);
-}
+Morris.Donut({
+  element: 'graph-teacher',
+  data: [
+    {value: 7.9, label: 'CUMPLY'},
+    {value: 2.1, label: 'FAILS'},
+  ],
+  backgroundColor: '#ccc',
+  labelColor: '#faa719',
+  colors: [
+    '#faa719',
+    '#ec6a0c',
+    '#f91b05',
+    '#95D7BB'
+  ],
+  formatter: function (x) { return x + "%"}
+});
 
-//funcion numero total de estudiantes
- var sumar = funcion(){
-     var students = data.AQP["2016-2"].students;
+//Octovo grafico jedi master rating
 
-     var counter = 0
-     for (var i = 0; i < students.length; i++){
-       counter = counter + 1
-     }
- }*/
-
-//funcion desercion
-/*var container = document.getElementById('container');
-var students = data.SCL["2016-2"].students;
- var result = 0;
-for (var i = 0; i < students.length -1; i++){
-
- if(students[i].active === false){
-   result = [i];
- }
-}
-
-
-var parrafo = document.createElement('p');
-var nodoParrafo = document.createTextNode(result);
-    parrafo.appendChild(nodoParrafo);
-    container.appendChild(parrafo);*/
-
-
-    //Variables
+Morris.Donut({
+  element: 'graph-jedi',
+  data: [
+    {value: 91, label: 'CUMPLY'},
+    {value: 9, label: 'FAILS'},
+  ],
+  backgroundColor: '#ccc',
+  labelColor: '#faa719',
+  colors: [
+    '#faa719',
+    '#ec6a0c',
+    '#f91b05',
+    '#95D7BB'
+  ],
+  formatter: function (x) { return x + "%"}
+});
 
 
-    //funciones
-/*    (function(){
-    var container = document.getElementById('container');
-    var students = data.SCL["2016-2"].students;
+//funciones
+
+(function(){
+
+    //Total de estudiantes generación 2017-2
+
+    var container = document.getElementById('enrolled');
+    var students = data.SCL["2017-2"].students;
 
         var total = students.length;
-        var parrafo = document.createElement('p');
-      	var nodoParrafo = document.createTextNode(total);
-      		parrafo.appendChild(nodoParrafo);
-      		container.appendChild(parrafo);
+        var cifra = document.createElement('h1');
+      	var nodoCifra = document.createTextNode(total);
+      		cifra.appendChild(nodoCifra);
+      		enrolled.appendChild(cifra);
 
-}());*/
+    //Deserción de estudiantes generación 2017-2
 
-    /*for (var i = 0; i < data.SCL['2016-2'].students.length; i++) {
-    	console.log(data.SCL['2016-2'].students[i].name);
-    	var parrafo = document.createElement('p');
-    	var nodoParrafo = document.createTextNode('La estudiante número ' + [i+1] + ' es: ' + data.SCL['2016-2'].students[i].name);
-    		parrafo.appendChild(nodoParrafo);
-    		container.appendChild(parrafo);
-    }*/
+    var container = document.getElementById('dropout');
+    var students = data.SCL["2017-2"].students;
+    var result = 0;
+        for (var i = 0; i < students.length; i++){
+
+         if(students[i].active === false);{
+               result = i++;
+         }
+        }
+    
+    //var porcentaje = (result * 100)/students.length
+
+        var porcentaje = document.createElement('h1');
+        var nodoPorcentaje = document.createTextNode(result);
+            porcentaje.appendChild(nodoPorcentaje);
+            dropout.appendChild(porcentaje);
+
+    //Promedio de los profesores generación 2017-2
+
+    var container = document.getElementById('teacher');
+    var sumaProf = 0;
+    var promProf = 0;
+        for(var i = 0; i < data.SCL['2017-2'].ratings.length; i++){
+        sumaProf += data.SCL['2017-2'].ratings[i].teacher;
+        promProf = sumaProf / data.SCL['2017-2'].ratings.length;
+        }
+
+        //console.log(sumaProf+' suma y promedio '+promProf);
+
+        var promedioProfes =  promProf;
+
+        var promedio = document.createElement('h1');
+        var nodoPromedio = document.createTextNode(promedioProfes);
+            promedio.appendChild(nodoPromedio);
+            teacher.appendChild(promedio);
+
+
+    //Promedio de los jedi master generación 2017-2
+
+    var container = document.getElementById('jedi');
+    var sumaJedi = 0;
+    var promJedi = 0;
+        for(var i = 0; i < data.SCL['2017-2'].ratings.length; i++){
+        sumaJedi += data.SCL['2017-2'].ratings[i].jedi;
+        promJedi = sumaJedi / data.SCL['2017-2'].ratings.length;
+        }
+
+        //console.log(sumaJedi+' suma y promedio '+promJedi);
+
+        var promedioJedis = promJedi;
+
+        var promedio = document.createElement('h1');
+        var nodoPromedio = document.createTextNode(promedioJedis);
+            promedio.appendChild(nodoPromedio);
+            jedi.appendChild(promedio);
+}());
